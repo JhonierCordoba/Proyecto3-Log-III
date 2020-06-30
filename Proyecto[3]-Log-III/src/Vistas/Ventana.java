32c7071d17212b3455a670b165619afc1033c5c9
JuanZea <61364624+JuanZea@users.noreply.github.com>
2020-06-30 15:51:18 -0500

    feat: Inicializacion de JavaFX

---
+++ Proyecto[3]-Log-III/src/Vistas/Ventana.java
@@ -0,0 +1,16 @@
+package Vistas;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Ventana extends Application {
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
