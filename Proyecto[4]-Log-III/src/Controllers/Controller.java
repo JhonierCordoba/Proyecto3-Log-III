0177a32458ff1ccff5e9e4888ecf4b4216c658bc
JuanZea <61364624+JuanZea@users.noreply.github.com>
2020-07-10 14:25:40 -0500

    Inicio Proyecto-4

---
+++ Proyecto[4]-Log-III/src/Controllers/Controller.java
@@ -0,0 +1,39 @@
+package Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    TextField in_n;
+    @FXML
+    TextField in_k;
+    
+    @FXML
+    TextField out_Cantidad;
+    
+    @FXML
+    TextField tb_1;
+    @FXML
+    TextField tb_2;
+    
+    @FXML
+    CheckBox cb_Rango;
+    
+    public void generarEntero(){
+    
+    }
+    public void generarVector(){
+    
+    }
+    public void Ordenar(){
+    
+    }
+    public void AlgoritmoNK(){
+    
+    }
+    public void calcularPrimos(){
+    
+    }
+}
